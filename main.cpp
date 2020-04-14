5147fb6f43e7fe18a56e9b02d780aeaba2e2e898
Louis de Benoist <louis.de-benoist-de-gentissart@polytechnique.edu>
2020-04-15 01:28:45 +0200

    code is cleaner

--- main.cpp
+++ main.cpp
@@ -89,14 +89,12 @@ ostream& operator<<(ostream &out, Vector const& v) {
     return out;
 };
 
-
 // Intersection structure 
 struct Intersection{
     bool intersection_exists; // bool saying if there's an intersection
     Vector P; // Closest point of intersection
     Vector N; // vector normal to point of intersection
 };
-
 
 // Ray Class Definition
 class Ray { 
@@ -125,6 +123,7 @@ class Sphere {
         Vector albedo; // rgb color of the sphere
         bool is_mirror; // whether it is a mirror
         int ID; // identification number
+        // double n; refraction index
 
         explicit Sphere(Vector center, double r, Vector color, bool is_a_mirror, int id){ 
             C = center;
@@ -148,18 +147,15 @@ class Sphere {
             struct Intersection inter;
             double discr = pow(dot(ray.u, ray.O - C), 2) - (dot(ray.O - C, ray.O -C) - pow(R,2));
             inter.intersection_exists = false;
+
             // if the discriminant is negative, there are no intersections
-            if (discr < 0){
-                return inter;
-            }
+            if (discr < 0) return inter;
             
             double t1 = dot(ray.u, C - ray.O) + sqrt(discr);
             double t2 = dot(ray.u, C - ray.O) - sqrt(discr);
 
             // we only consider the root with t > 0
-            if (std::max(t1,t2) < 0.){
-                return inter;
-            }
+            if (std::max(t1,t2) < 0.) return inter;
         
             double t = std::min(t1,t2); // keep the smallest positive root (we want the closest one)
             inter.intersection_exists = true;
@@ -215,7 +211,7 @@ class Scene {
                     }
                 }
             }
-            
+
             intersection.P = P;
             intersection.N = N;
             intersection.s = closest_sphere; 
@@ -246,9 +242,8 @@ Vector getColor(Ray& ray, Scene& scene, Vector& light_source, double& I, int max
     Vector P = scene_intersection.P; 
     Vector N = scene_intersection.N;
 
-    double eps = pow(10, -4); 
-    P  = P + eps*N; // Offset P (in case of noise)
-
+    double eps = pow(10, -4); P  = P + eps*N; // Offset P (in case of noise)
+    
     // Check if the the closest sphere is a mirror or not
     if (s.is_mirror){
         Vector reflected_dir = ray.u - 2*(dot(ray.u, N)*N);
@@ -257,19 +252,20 @@ Vector getColor(Ray& ray, Scene& scene, Vector& light_source, double& I, int max
         return getColor(reflected_ray, scene, light_source, I, max_depth-1);
     }
     else {   
-        // Find omega and the light ray. Omega is the vector pointing from P in the direction of the light source
+        // Find omega and the light ray. Omega is the vector pointing from P in the direction of the light source. d is the distance from P to the light source
         Vector omega = light_source - P ; omega.normalize();
         Ray omega_ray = Ray(P, omega); 
-        bool is_visible; 
+        double d = norm(light_source-P); 
 
-        // Visible if the ray has no intersection closer ||S-P||, i.e. nothing is blocking it from 
-        // reaching the light source
-        if (norm(scene.intersection(omega_ray).P - P) > norm(light_source-P)){is_visible = true;}
-        else{is_visible = false;}
-
-        Vector lambertian_reflection_intensity = I / (4*M_PI*dot(light_source-P, light_source-P)) * (1/M_PI) * s.albedo * is_visible * std::max(0., dot(N, omega));
-    
-        return lambertian_reflection_intensity;
+        // if omega_ray has no intersection closer than ||light_source-P||, i.e. nothing is blocking it from reaching the light source
+        if (norm(scene.intersection(omega_ray).P - P) > d){
+            // return the color of the pixel
+            return I / (4*M_PI*pow(d, 2)) * (1/M_PI) * s.albedo * std::max(0., dot(N, omega));
+        }
+        else {
+            // if it is not visible, the pixel is black (shadow)
+            return Vector(0,0,0);
+        }
     }
 };
 
@@ -277,18 +273,12 @@ int main(){
     int W = 1000 ; int H = 1000; // width and height of image
     unsigned char image[W * H * 3]; //image using row major ordering
 
-    /*
-    Red pixel value at (x,y):   image[y*W*3 + x*3 + 0]
-    Green pixel value at (x,y): image[y*W*3 + x*3 + 1]
-    Blue pixel value at (x,y):  image[y*W*3 + x*3 + 2]
-    */
-
     // Define the walls
     Sphere wall1 = Sphere(Vector(0,-1000,0), 990, Vector(0, 0, 1), false, 1);
     Sphere wall2 = Sphere(Vector(0, 0, -1000), 940, Vector(0, 1, 0), false, 2);
     Sphere wall3 = Sphere(Vector(0, 1000, 0), 940, Vector(1, 0, 0), false, 3);
     Sphere wall4 = Sphere(Vector(0, 0, 1000), 940, Vector(0.5, 0, 0.5), false, 4);
-    Sphere wall5 = Sphere(Vector(1000, 0, 0), 940, Vector(0.5, 0.7, 0.9), false, 5);
+    Sphere wall5 = Sphere(Vector(1000, 0, 0), 940, Vector(0.5, 0.6, 0.9), false, 5);
     Sphere wall6 = Sphere(Vector(-1000, 0, 0), 940, Vector(1, 1, 0), false, 6);
 
     // Define the light source
@@ -298,16 +288,16 @@ int main(){
     // gamma for gamma correction
     double gamma = 2.2; 
 
-    // initialize camera at (0,0,55)
+    // initialize camera location
     Vector camera = Vector(0, 0, 70);
 
-    // Set the horizontal field of view to 60 degrees
+    // Set the horizontal field of view
     double fov = 60.0;
 
-    // Get distance from the camera to the screen (pixel grid)
+    // Get the distance from the camera to the screen (pixel grid)
     double dist_from_screen = W / (2*tan( (fov * M_PI / 180) / 2));
 
-    // put some spheres in the room
+    // put some objects in the room
     Sphere sphere1 = Sphere(Vector(0,0,0), 10, Vector(1, 1, 1), true, 7);
     Sphere sphere2 = Sphere(Vector(25, 0,0), 10, Vector(1, 1, 1), false, 8);
     Sphere sphere3 = Sphere(Vector(-15, -5, 30), 5, Vector(1, 1, 0), false, 9);
@@ -325,25 +315,21 @@ int main(){
     objects_in_room.push_back(wall6);
     Scene scene = Scene(objects_in_room);
 
+    int max_depth = 5; 
     for (int x = 0 ; x < W ; x++){
         for (int y = 0 ; y < H ; y++){
             // For each pixel (x,y)
             Vector direction = pixel_to_coordinate(camera, x, y, dist_from_screen, H, W);
-            // find the ray that passes through the pixel
+            // find the ray that passes through the pixel, starting from the camera
             Ray ray = Ray(camera, direction);
-        
             // get the pixel's color
-            int max_depth = 5; 
             Vector pixel_color = getColor(ray, scene, light_source, I, max_depth);
-
             // gamma correction
             pixel_color[0] = pow(pixel_color[0], 1/gamma);
             pixel_color[1] = pow(pixel_color[1], 1/gamma);
             pixel_color[2] = pow(pixel_color[2], 1/gamma);
-
             // scaling
             pixel_color = 255 * pixel_color; 
-
             // update the pixel on the image
             image[y*W*3 + x*3 + 0] = std::min(255., pixel_color[0]);
             image[y*W*3 + x*3 + 1] = std::min(255., pixel_color[1]);
@@ -351,7 +337,6 @@ int main(){
             
         }
     }
-
     // save the image
     stbi_write_jpg("room.jpg", W, H, 3, image, W * sizeof(int));
 
