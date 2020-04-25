6182c8471ab44d77299d60634e158137ecf5bfb3
Louis de Benoist <louis.de-benoist-de-gentissart@polytechnique.edu>
2020-04-25 17:50:06 +0200

    added abstract Geometry class

--- main.cpp
+++ main.cpp
@@ -8,10 +8,6 @@
 #include <time.h>
 #include <chrono> 
 using namespace std::chrono; 
-using namespace std;
-
-int NUM_CALLS = 0; 
-double EXEC_TIME = 0; 
 
 // Vector Class Definition
 class Vector { 
@@ -58,6 +54,10 @@ class Vector {
             coords[2] = coords[2] / norm;
             return *this ;
         };
+
+        std::string to_string(){
+           return "Vector(" + std::to_string(coords[0]) + "," + std::to_string(coords[1]) + "," + std::to_string(coords[2]) + ")";
+        }
 };
 
 // Vector Operators
@@ -100,15 +100,9 @@ Vector operator*(double lambda, const Vector& v) {
     return Vector(v[0]*lambda, v[1]*lambda, v[2]*lambda);
 };
 
-// printing
-ostream& operator<<(ostream &out, Vector const& v) {
-    out << "Vector(" << v[0] << "," << v[1] << "," << v[2] << ")";
-    return out;
-};
-
 // Intersection structure 
 struct Intersection{
-    bool intersection_exists; // bool saying if there's an intersection
+    bool exists; // bool saying if there's an intersection
     Vector P; // Closest point of intersection
     Vector N; // vector normal to point of intersection
 };
@@ -120,7 +114,7 @@ class Ray {
         Vector u; // Direction (unit vector)
 
         explicit Ray(Vector origin, Vector direction){
-            direction.normalize();
+            //direction.normalize(); //for speed, assume that direction is already normalized
             O = origin;
             u = direction;
         };
@@ -128,25 +122,29 @@ class Ray {
         // default constructor if no arguments are given
         Ray(){
             O = Vector();
-            u = Vector(); u.normalize();
+            u = Vector();
+        }
+
+        std::string to_string(){
+            return "Ray(" + O.to_string() + "," + u.to_string() + ")";
         }
 };
 
-//printing
-ostream& operator<<(ostream &out, Ray const& ray) {
-    out << "Ray(" << ray.O << "," << ray.u << ")";
-    return out;
-};
+// Geometry Class Definition
+class Geometry{
+    public:
+        virtual Intersection intersection(Ray ray) = 0;
+        virtual std::string to_string() = 0; 
+        bool is_mirror; 
+        Vector albedo; 
+        int ID; 
+};  
 
 // Sphere Class Definition
-class Sphere { 
+class Sphere : public Geometry { 
     public:
         Vector C; // center
         double R; // radius
-        Vector albedo; // rgb color of the sphere
-        bool is_mirror; // whether it is a mirror
-        int ID; // identification number
-        // double n; refraction index
 
         explicit Sphere(Vector center, double r, Vector color, bool is_a_mirror, int id){ 
             C = center;
@@ -162,14 +160,14 @@ class Sphere {
             R = 0; 
             albedo = Vector(0,0,0); 
             is_mirror = false; 
-            ID = 0;
+            ID = -1;
         }
 
         // Where does the sphere intersect with a given ray?
         Intersection intersection(Ray ray){
             struct Intersection inter;
             double discr = pow(dot(ray.u, ray.O - C), 2) - (dot(ray.O - C, ray.O -C) - pow(R,2));
-            inter.intersection_exists = false;
+            inter.exists = false;
 
             // if the discriminant is negative, there are no intersections
             if (discr < 0) return inter;
@@ -181,7 +179,7 @@ class Sphere {
             if (std::max(t1,t2) < 0.) return inter;
         
             double t = std::min(t1,t2); // keep the smallest positive root (we want the closest one)
-            inter.intersection_exists = true;
+            inter.exists = true;
             Vector P = ray.O + t*ray.u;
 
             // Find the normal vector to the sphere at P
@@ -192,65 +190,62 @@ class Sphere {
 
             return inter;
         };
-};
 
-// Printing
-ostream& operator<<(ostream &out, Sphere const& s) {
-    out << "Sphere(ID=" << s.ID <<")";
-    return out;
+        std::string to_string(){
+            return "Sphere(ID=" + std::to_string(ID) + ")"; 
+        };
 };
 
 struct Scene_Intersection : Intersection{
-    Sphere s; // the object being intersected
+    int object_ID; // the object being intersected
 };
 
 // Scene Class Definition
 class Scene { 
     public:
-        std::vector<Sphere> spheres;
+        std::vector<Geometry*> objects;
 
-        explicit Scene(std::vector<Sphere>& sphere_list){
-            spheres = sphere_list;
+        explicit Scene(std::vector<Geometry*>& objects_list){
+            objects = objects_list;
         };
 
         Scene_Intersection intersection(Ray ray){
-            struct Scene_Intersection intersection;
-            intersection.intersection_exists = false; // default
+            struct Scene_Intersection scene_intersection;
+            scene_intersection.exists = false; // default
             double smallest_distance = std::numeric_limits<double>::infinity(); //initalize as infinity
-            Sphere closest_sphere = Sphere();
             Vector P; //closest point
             Vector N; 
+            int closest_object_ID = 0; 
 
-            for (auto s : spheres){
-                Intersection inter = s.intersection(ray);
-                if (inter.intersection_exists){
-                    intersection.intersection_exists = true;
+            for (int i = 0; i < objects.size(); i++){
+                Intersection inter = objects[i] -> intersection(ray);
+                if (inter.exists){
+                    scene_intersection.exists = true;
                     double distance = norm(ray.O - inter.P); 
                     if (distance < smallest_distance){
                         smallest_distance = distance;
                         P = inter.P;
                         N = inter.N;
-                        closest_sphere = s; 
+                        closest_object_ID = objects[i] -> ID; 
                     }
                 }
             }
-
-            intersection.P = P;
-            intersection.N = N;
-            intersection.s = closest_sphere; 
-            return intersection; 
+            scene_intersection.P = P;
+            scene_intersection.N = N;
+            scene_intersection.object_ID = closest_object_ID; 
+            return scene_intersection; 
         }
+
+        std::string to_string(){
+            std::string res = "";
+            for (int i = 0; i < objects.size(); i++){
+                res += objects[i] -> to_string();
+            }
+            res += "}\n"; return res;
+        };
 };
 
-ostream& operator<<(ostream &out, Scene const& scene) {
-    out << "Scene{";
-    for (std::vector<Sphere>::const_iterator i = scene.spheres.begin(); i != scene.spheres.end(); ++i)
-        out << *i << ',' << "\n";
-    out << "}\n";
-    return out;
-};
-
-Vector pixel_to_coordinate(Vector camera, double x, double y, double dist_from_screen, int H, int W){
+Vector pixel_to_coordinate(Vector& camera, double x, double y, double dist_from_screen, int H, int W){
     return Vector(camera[0] + x + 0.5 - W/2, camera[1] + y + 0.5 - H/2, camera[2] - dist_from_screen);
 };
 
@@ -288,22 +283,31 @@ Vector random_cos(Vector N){
     return x*T1 + y*T2 + z*N; 
 }
 
-Vector getColor(Ray& ray, Scene& scene, Vector& light_source, double& I, int max_depth){
-    NUM_CALLS += 1; 
+void boxMuller(double standard_deviation, double &x, double &y){
+    // get r1,r2 ~ U([0,1])
+    double r1 = ((double) rand() / (RAND_MAX));
+    double r2 = ((double) rand() / (RAND_MAX));
+
+    x = sqrt(-2 * log(r1))*cos(2 * M_PI*r2)*standard_deviation; 
+    y = sqrt(-2 * log(r1))*sin(2 * M_PI*r2)*standard_deviation;
+}
+
+Vector getColor(Ray& ray, Scene* scene, Vector* light_source, double& I, int max_depth){
     if (max_depth == 0){
         return Vector(0,0,0); // if maximum recusive depth is reached
     }
 
     // Find the intersection with the closest sphere, s, and get the point, P, and the vector normal to it, N
-    Scene_Intersection scene_intersection = scene.intersection(ray);
-    Sphere s = scene_intersection.s;
+    Scene_Intersection scene_intersection = scene->intersection(ray);
+    int index = scene_intersection.object_ID; // index of the object in the scene objects vector is object's ID
+    
     Vector P = scene_intersection.P; 
     Vector N = scene_intersection.N;
 
     double eps = pow(10, -4); P  = P + eps*N; // Offset P (in case of noise)
     
     // Check if the the closest sphere is a mirror or not
-    if (s.is_mirror){
+    if (scene->objects[index]->is_mirror){
         Vector reflected_dir = ray.u - 2*(dot(ray.u, N)*N);
         Ray reflected_ray = Ray(P, reflected_dir);
         // find the color of the object that the reflected ray ends up hitting
@@ -312,16 +316,16 @@ Vector getColor(Ray& ray, Scene& scene, Vector& light_source, double& I, int max
     else {   
         // DIRECT LIGHTING
         // Find omega and the light ray. Omega is the vector pointing from P in the direction of the light source. d is the distance from P to the light source
-        Vector omega = light_source - P ; omega.normalize();
+        Vector omega = *light_source - P ; omega.normalize();
         Ray omega_ray = Ray(P, omega); 
-        double d = norm(light_source-P); 
+        double d = norm(*light_source-P); 
 
         Vector L0; 
 
         // if omega_ray has no intersection closer than ||light_source-P||, i.e. nothing is blocking it from reaching the light source
-        if (norm(scene.intersection(omega_ray).P - P) > d){
+        if (norm(scene->intersection(omega_ray).P - P) > d){
             // return the color of the pixel
-            L0 =  I / (4*M_PI*pow(d, 2)) * (1/M_PI) * s.albedo * std::max(0., dot(N, omega));
+            L0 =  I / (4*M_PI*pow(d, 2)) * (1/M_PI) * scene->objects[index]->albedo * std::max(0., dot(N, omega));
         }
         else {
             // if it is not visible, the pixel is black (shadow)
@@ -329,12 +333,13 @@ Vector getColor(Ray& ray, Scene& scene, Vector& light_source, double& I, int max
         }
 
         // COMBINE WITH INDIRECT LIGHTING
-        //Ray random_ray = Ray(P, random_cos(N));
-        //L0 = L0 + mult(s.albedo, getColor(random_ray, scene, light_source, I, max_depth-1)); 
+        Ray random_ray = Ray(P, random_cos(N));
+        L0 = L0 + mult(scene->objects[index]->albedo, getColor(random_ray, scene, light_source, I, max_depth-1)); 
     
         return L0;
     }
 };
+
 
 int main(){
     auto start = high_resolution_clock::now(); // to measure execution time
@@ -343,16 +348,16 @@ int main(){
     int W = 500 ; int H = 500; // width and height of image
     unsigned char image[W * H * 3]; //image using row major ordering
     
-    // Define the walls
-    Sphere wall1 = Sphere(Vector(0,-1000,0), 990, Vector(0, 0, 1), false, 1);
-    Sphere wall2 = Sphere(Vector(0, 0, -1000), 940, Vector(0, 1, 0), false, 2);
-    Sphere wall3 = Sphere(Vector(0, 1000, 0), 940, Vector(1, 0, 0), false, 3);
-    Sphere wall4 = Sphere(Vector(0, 0, 1000), 940, Vector(0.5, 0, 0.5), false, 4);
-    Sphere wall5 = Sphere(Vector(1000, 0, 0), 940, Vector(0.5, 0.6, 0.9), false, 5);
-    Sphere wall6 = Sphere(Vector(-1000, 0, 0), 940, Vector(1, 1, 0), false, 6);
+    // Define the walls (pointers to big spheres)
+    Sphere* wall1 = new Sphere(Vector(0,-1000,0), 990, Vector(0, 0, 1), false, 0);
+    Sphere* wall2 = new Sphere(Vector(0, 0, -1000), 940, Vector(0, 1, 0), false, 1);
+    Sphere* wall3 = new Sphere(Vector(0, 1000, 0), 940, Vector(1, 0, 0), false, 2);
+    Sphere* wall4 = new Sphere(Vector(0, 0, 1000), 940, Vector(0.5, 0, 0.5), false, 3);
+    Sphere* wall5 = new Sphere(Vector(1000, 0, 0), 940, Vector(0.5, 0.6, 0.9), false, 4);
+    Sphere* wall6 = new Sphere(Vector(-1000, 0, 0), 940, Vector(1, 1, 0), false, 5);
 
     // Define the light source
-    Vector light_source = Vector(-10, 20, 40);
+    Vector* light_source = new Vector(-10, 20, 40);
     double I = 2*pow(10,5) ; // light intensity
 
     // gamma for gamma correction
@@ -368,42 +373,41 @@ int main(){
     double dist_from_screen = W / (2*tan( (fov * M_PI / 180) / 2));
 
     // put some objects in the room
-    Sphere sphere1 = Sphere(Vector(0,0,0), 10, Vector(1, 1, 1), true, 7);
-    Sphere sphere2 = Sphere(Vector(25, 0,0), 10, Vector(1, 1, 1), false, 8);
-    Sphere sphere3 = Sphere(Vector(-15, -5, 30), 5, Vector(1, 1, 0), false, 9);
+    Sphere* sphere1 = new Sphere(Vector(0,0,0), 10, Vector(1, 1, 1), true, 6);
+    Sphere* sphere2 = new Sphere(Vector(25, 0,0), 10, Vector(1, 1, 1), false, 7);
+    Sphere* sphere3 = new Sphere(Vector(-15, -5, 30), 5, Vector(1, 1, 0), false, 8);
 
     // Set the scene
-    std::vector<Sphere> objects_in_room; 
-    objects_in_room.push_back(sphere1);
-    objects_in_room.push_back(sphere2);
-    objects_in_room.push_back(sphere3);
+    std::vector<Geometry*> objects_in_room; 
     objects_in_room.push_back(wall1);
     objects_in_room.push_back(wall2);
     objects_in_room.push_back(wall3);
     objects_in_room.push_back(wall4);
     objects_in_room.push_back(wall5);
     objects_in_room.push_back(wall6);
-    Scene scene = Scene(objects_in_room);
+    objects_in_room.push_back(sphere1);
+    objects_in_room.push_back(sphere2);
+    objects_in_room.push_back(sphere3);
+    Scene* scene = new Scene(objects_in_room);
 
     int max_depth = 5; 
     int num_paths = 32; 
+    double stdv = 0.5; 
 
     #pragma omp parallel for schedule(dynamic, 1)
     for (int x = 0 ; x < W ; x++){
         for (int y = 0 ; y < H ; y++){
-            // For each pixel (x,y)
-            Vector direction = pixel_to_coordinate(camera, x, y, dist_from_screen, H, W);
-            // find the ray that passes through the pixel, starting from the camera
-            Ray ray = Ray(camera, direction);
             // get the pixel's color
             Vector pixel_color;
 
             for (int k = 0 ; k < num_paths ; k++){
-                 auto s = high_resolution_clock::now(); 
-                 pixel_color = pixel_color + getColor(ray, scene, light_source, I, max_depth);
-                 auto end = high_resolution_clock::now();
-                 auto d = duration_cast<microseconds>(end - s); 
-                 EXEC_TIME += d.count();
+                // For each pixel (x,y)
+                double eps ; double eta; boxMuller(stdv, eps, eta); // add random fluctuation
+                Vector random_direction = pixel_to_coordinate(camera, x+eps, y+eta, dist_from_screen, H, W); random_direction.normalize();
+
+                // find the ray that passes through the pixel, starting from the camera
+                Ray ray = Ray(camera, random_direction);
+                pixel_color = pixel_color + getColor(ray, scene, light_source, I, max_depth);
             }
 
             pixel_color = pow(num_paths,-1) * pixel_color; 
@@ -425,10 +429,6 @@ int main(){
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start); 
     std::cout << "Rendering time: " << duration.count()*pow(10, -6) << " seconds\n"; 
-    std::cout << "getColor time: " << EXEC_TIME*pow(10, -6) << "\n";
-    cout << "getColor number of calls: " << NUM_CALLS << "\n"; 
-    cout << "Average running time getColor: " << EXEC_TIME*pow(10, -6) / NUM_CALLS << "\n";
-
     return 0;
 };
 
