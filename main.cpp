306bdf78af047fbe45554e348eea16d9ea811276
Louis de Benoist <louis.de-benoist-de-gentissart@polytechnique.edu>
2020-05-02 00:03:16 +0200

    bounding box working

--- main.cpp
+++ main.cpp
@@ -20,6 +20,9 @@ Rendering using Ray-Tracing
 #include <fstream>
 using namespace std::chrono; 
 
+
+const double INF = std::numeric_limits<double>::infinity(); 
+
 /* ------------------------------------------------------------------------------------------------------------------------
 
 Vector Class Definition
@@ -27,42 +30,39 @@ Vector Class Definition
 ------------------------------------------------------------------------------------------------------------------------ */
 
 class Vector { 
-    private:
-        double coords[3];
     public:
         ~Vector() {}
-        explicit Vector(double x = 0. , double y = 0. , double z = 0.){ 
-            coords[0] = x;
-            coords[1] = y;
-            coords[2] = z;
-        };
-        const double& operator[](int i) const{
-            return coords[i];
-        };
-        double& operator[](int i){
-            return coords[i];
+
+        double x;
+        double y; 
+        double z; 
+
+        explicit Vector(double x_ = 0. , double y_ = 0. , double z_ = 0.){ 
+            x = x_;
+            y = y_;
+            z = z_;
         };
         Vector& operator+=(const Vector& v) {
-            coords[0] += v[0]; 
-            coords[1] += v[1]; 
-            coords[2] += v[2]; 
+            x += v.x; 
+            y += v.y; 
+            z += v.z; 
             return *this ;
         };
         Vector& operator-=(const Vector& v) {
-            coords[0] -= v[0]; 
-            coords[1] -= v[1]; 
-            coords[2] -= v[2]; 
+            x -= v.x; 
+            y -= v.y; 
+            z -= v.z; 
             return *this ;
         };
         Vector& normalize() {
-            double norm = sqrt(coords[0]*coords[0] + coords[1]*coords[1] + coords[2]*coords[2]);
-            coords[0] = coords[0] / norm;
-            coords[1] = coords[1] / norm;
-            coords[2] = coords[2] / norm;
+            double norm = sqrt(x*x + y*y + z*z);
+            x = x / norm;
+            y = y / norm;
+            z = z / norm;
             return *this ;
         };
         std::string to_string(){
-           return "Vector(" + std::to_string(coords[0]) + "," + std::to_string(coords[1]) + "," + std::to_string(coords[2]) + ")";
+           return "Vector(" + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(z) + ")";
         }
 };
 
@@ -81,28 +81,28 @@ Vector Operators
 ------------------------------------------------------------------------------------------------------------------------ */
 
 Vector operator+(const Vector& v, const Vector& w){
-    return Vector(v[0] + w[0], v[1] + w[1], v[2] + w[2]);
+    return Vector(v.x + w.x, v.y + w.y, v.z + w.z);
 };
 Vector operator-(const Vector& v, const Vector& w){
-    return Vector(v[0] - w[0], v[1] - w[1], v[2] - w[2]);
+    return Vector(v.x - w.x, v.y - w.y, v.z - w.z);
 };
 double dot(const Vector& v, const Vector& w){
-    return v[0]*w[0] + v[1]*w[1] + v[2]*w[2];
+    return v.x *w.x + v.y*w.y + v.z*w.z;
 };
 Vector cross(const Vector& v, const Vector& w){
-    return Vector(v[1]*w[2] - v[2]*w[1], v[2]*w[0] - v[0]*w[2], v[0]*w[1] - v[1]*w[0]);
+    return Vector(v.y*w.z - v.z*w.y, v.z*w.x - v.x*w.z, v.x*w.y - v.y*w.x);
 };
 Vector mult(const Vector& v, const Vector& w){
-    return Vector(v[0]*w[0], v[1]*w[1], v[2]*w[2]);
+    return Vector(v.x*w.x, v.y*w.y, v.z*w.z);
 };
 double norm(const Vector& v){
     return sqrt(dot(v, v));
 };
 Vector operator*(const Vector& v, double lambda) {
-    return Vector(v[0]*lambda, v[1]*lambda, v[2]*lambda);
+    return Vector(v.x*lambda, v.y*lambda, v.z*lambda);
 };
 Vector operator*(double lambda, const Vector& v) {
-    return Vector(v[0]*lambda, v[1]*lambda, v[2]*lambda);
+    return Vector(v.x*lambda, v.y*lambda, v.z*lambda);
 };
 
 
@@ -152,34 +152,6 @@ struct Intersection{
 
 /* ------------------------------------------------------------------------------------------------------------------------
 
-Plane Class
-
------------------------------------------------------------------------------------------------------------------------- */
-
-class Plane{
-public:
-    Vector A;
-    Vector N; 
-
-    Plane(Vector A_, Vector N_){
-        A = A_;
-        N = N_;
-    }
-
-    Intersection intersection(Ray ray){
-        double t = dot(A-ray.O, N) / dot(ray.u, N);
-        Intersection inter; inter.exists = false; 
-        if (t > 0){
-            inter.exists = true; 
-            inter.t = t; 
-        }
-        return inter; 
-    }
-};
-
-
-/* ------------------------------------------------------------------------------------------------------------------------
-
 Geometry Class Definition
 
 ------------------------------------------------------------------------------------------------------------------------ */
@@ -210,7 +182,7 @@ class Sphere : public Geometry {
             C = center;
             R = r;
             albedo = color;
-            if (material == "mirror"){is_mirror = true;} else {is_mirror = false;}
+            is_mirror = (material == "mirror");
             is_mesh = false;
             ID = id; 
         };
@@ -276,42 +248,30 @@ public:
 
     // returns whether the ray intersects the bounding box
     bool intersection(Ray ray){
-        // Planes where x is constant
-        Intersection inter1 = Plane(Bmin, Vector(1,0,0)).intersection(ray); 
-        Intersection inter2 = Plane(Bmax, Vector(1,0,0)).intersection(ray);
 
-        // Planes where y is constant
-        Intersection inter3 = Plane(Bmin, Vector(0,1,0)).intersection(ray);
-        Intersection inter4 = Plane(Bmax, Vector(0,1,0)).intersection(ray);
+        Vector dmin = Bmin - ray.O, dmax = Bmax - ray.O; // since we will use this value a lot
 
-        // Planes where z is constant
-        Intersection inter5 = Plane(Bmin, Vector(0,0,1)).intersection(ray);
-        Intersection inter6 = Plane(Bmax, Vector(0,0,1)).intersection(ray);
+        // for saving computation time, we simplify the inner products
+        double t1 = dmin.x / ray.u.x; // dot(Bmin - ray.O, Vector(1,0,0)) / dot(ray.u, N)
+        double t2 = dmax.x / ray.u.x; // dot(Bmax - ray.O, Vector(1,0,0)) / dot(ray.u, N)
+        double t3 = dmin.y / ray.u.y; // dot(Bmin - ray.O, Vector(0,1,0)) / dot(ray.u, N)
+        double t4 = dmax.y / ray.u.y; // dot(Bmax - ray.O, Vector(0,1,0)) / dot(ray.u, N)
+        double t5 = dmin.z / ray.u.z; // dot(Bmin - ray.O, Vector(0,0,1)) / dot(ray.u, N)
+        double t6 = dmax.z / ray.u.z; // dot(Bmax - ray.O, Vector(0,0,1)) / dot(ray.u, N)
+       
+        double tx_0 = -INF, tx_1 = INF, ty_0 = -INF, ty_1 = INF, tz_0 = -INF, tz_1 = INF; 
 
-        double tx_0 = - std::numeric_limits<double>::infinity(); 
-        double tx_1 = std::numeric_limits<double>::infinity(); 
-        double ty_0 = - std::numeric_limits<double>::infinity(); 
-        double ty_1 = std::numeric_limits<double>::infinity(); 
-        double tz_0 = - std::numeric_limits<double>::infinity(); 
-        double tz_1 = std::numeric_limits<double>::infinity(); 
-        
-        if (inter1.exists && inter2.exists){
-            tx_0 = std::min(inter1.t, inter2.t);
-            tx_1 = std::max(inter1.t, inter2.t); 
-        };
-        if (inter3.exists && inter4.exists){
-            ty_0 = std::min(inter3.t, inter4.t);
-            ty_1 = std::max(inter3.t, inter4.t); 
-        };
-        if (inter5.exists && inter6.exists){
-            tz_0 = std::min(inter5.t, inter6.t);
-            tz_1 = std::max(inter5.t, inter6.t); 
-        };
+        tx_0 = std::min(t1, t2);
+        tx_1 = std::max(t1, t2); 
+        ty_0 = std::min(t3, t4);
+        ty_1 = std::max(t3, t4); 
+        tz_0 = std::min(t5, t6);
+        tz_1 = std::max(t5, t6); 
 
         double l0[] = {tx_0, ty_0, tz_0};
         double l1[] = {tx_1, ty_1, tz_1};
-        if (*std::min_element(l1,l1+3) > *std::max_element(l0,l0+3)){return true;}
-        return false;
+
+        return *std::min_element(l1,l1+3) > *std::max_element(l0,l0+3);
     };
 };
 
@@ -334,46 +294,22 @@ public:
 
 class TriangleMesh : public Geometry {
 public:
-    ~TriangleMesh() {}
-	TriangleMesh() {
-        is_mesh = true; 
-    };
-
-    void resize(double scaling, Vector translation){
-        for (int i = 0; i < vertices.size(); i++){
-            vertices[i] = scaling*vertices[i] + translation;
-        }
-    }
-
     std::vector<TriangleIndices> indices;
-	std::vector<Vector> vertices;
-	std::vector<Vector> normals;
-	std::vector<Vector> uvs;
-	std::vector<Vector> vertexcolors;
-
-    std::string to_string(){
-        // print the vertices
-        return "TriangleMesh with " + std::to_string(vertices.size()) + " vertices\n";
-    };
+	std::vector<Vector> vertices, normals, uvs, vertexcolors;
 
     // return bounding box for the mesh
     BoundingBox getBoundingBox(){
-       double min_x = std::numeric_limits<double>::infinity(); 
-       double min_y = std::numeric_limits<double>::infinity(); 
-       double min_z = std::numeric_limits<double>::infinity(); 
-
-       double max_x = -std::numeric_limits<double>::infinity();
-       double max_y = -std::numeric_limits<double>::infinity();
-       double max_z = -std::numeric_limits<double>::infinity();
+       double min_x = INF, min_y = INF, min_z = INF, max_x = -INF, max_y = -INF, max_z = -INF;
 
        for (auto v : vertices){
-           if (v[0] < min_x) min_x = v[0];
-           if (v[0] > max_x) max_x = v[0];
-           if (v[1] < min_y) min_y = v[1];
-           if (v[1] > max_y) max_y = v[1];
-           if (v[2] < min_z) min_z = v[2];
-           if (v[2] > max_z) max_z = v[2];
+           if (v.x < min_x) min_x = v.x;
+           if (v.x > max_x) max_x = v.x;
+           if (v.y < min_y) min_y = v.y;
+           if (v.y > max_y) max_y = v.y;
+           if (v.z < min_z) min_z = v.z;
+           if (v.z > max_z) max_z = v.z;
        }
+
        Vector Bmin = Vector(min_x, min_y, min_z);
        Vector Bmax = Vector(max_x, max_y, max_z);
        return BoundingBox(Bmin, Bmax);
@@ -383,22 +319,16 @@ public:
         Intersection inter; inter.exists = false; 
 
         // first, check if the ray intersects the bounding box
-        BoundingBox box = getBoundingBox();
-        if (box.intersection(ray) == false){
-            return inter; 
-        }
+        if (getBoundingBox().intersection(ray) == false) return inter;
 
-        double smallest_t = std::numeric_limits<double>::infinity(); //initalize as infinity
         TriangleIndices intersected_triangle; 
-        double alpha_intersected; double beta_intersected; double gamma_intersected; 
+        double smallest_t = INF; //initalize as infinity
+        double alpha_intersected, beta_intersected, gamma_intersected; 
 
         for (auto triangle : indices){
             // vertices of the triangle associated to the index
-            Vector A = vertices[triangle.vtxi]; 
-            Vector B = vertices[triangle.vtxj]; 
-            Vector C = vertices[triangle.vtxk]; 
-
-            Vector e1 = B-A ; Vector e2 = C-A; 
+            Vector A = vertices[triangle.vtxi], B = vertices[triangle.vtxj], C = vertices[triangle.vtxk]; 
+            Vector e1 = B-A, e2 = C-A; 
             Vector non_normalized_normal = cross(e1, e2);
 
             // compute t
@@ -432,6 +362,22 @@ public:
 
         return inter; 
     }
+
+    ~TriangleMesh() {}
+	TriangleMesh() {
+        is_mesh = true; 
+    };
+
+    void resize(double scaling, Vector translation){
+        for (int i = 0; i < vertices.size(); i++){
+            vertices[i] = scaling*vertices[i] + translation;
+        }
+    }
+
+    std::string to_string(){
+        // print the vertices
+        return "TriangleMesh with " + std::to_string(vertices.size()) + " vertices\n";
+    };
 	
 	void readOBJ(const char* filename) {
 
@@ -458,27 +404,27 @@ public:
 				Vector vec;
 
 				Vector col;
-				if (sscanf(line, "v %lf %lf %lf %lf %lf %lf\n", &vec[0], &vec[1], &vec[2], &col[0], &col[1], &col[2]) == 6) {
-					col[0] = std::min(1., std::max(0., col[0]));
-					col[1] = std::min(1., std::max(0., col[1]));
-					col[2] = std::min(1., std::max(0., col[2]));
+				if (sscanf(line, "v %lf %lf %lf %lf %lf %lf\n", &vec.x, &vec.y, &vec.z, &col.x, &col.y, &col.z) == 6) {
+					col.x = std::min(1., std::max(0., col.x));
+					col.y = std::min(1., std::max(0., col.y));
+					col.z = std::min(1., std::max(0., col.z));
 
 					vertices.push_back(vec);
 					vertexcolors.push_back(col);
 
 				} else {
-					sscanf(line, "v %lf %lf %lf\n", &vec[0], &vec[1], &vec[2]);
+					sscanf(line, "v %lf %lf %lf\n", &vec.x, &vec.y, &vec.z);
 					vertices.push_back(vec);
 				}
 			}
 			if (line[0] == 'v' && line[1] == 'n') {
 				Vector vec;
-				sscanf(line, "vn %lf %lf %lf\n", &vec[0], &vec[1], &vec[2]);
+				sscanf(line, "vn %lf %lf %lf\n", &vec.x, &vec.y, &vec.z);
 				normals.push_back(vec);
 			}
 			if (line[0] == 'v' && line[1] == 't') {
 				Vector vec;
-				sscanf(line, "vt %lf %lf\n", &vec[0], &vec[1]);
+				sscanf(line, "vt %lf %lf\n", &vec.x, &vec.y);
 				uvs.push_back(vec);
 			}
 			if (line[0] == 'f') {
@@ -634,7 +580,7 @@ class Scene {
         Scene_Intersection intersection(Ray ray){
             struct Scene_Intersection scene_intersection;
             scene_intersection.exists = false; // default
-            double smallest_distance = std::numeric_limits<double>::infinity(); //initalize as infinity
+            double smallest_distance = INF; //initalize as infinity
             Vector P; //closest point
             Vector N; 
             Geometry* closest_object;
@@ -675,7 +621,7 @@ Get the coordinates of a pixel
 ------------------------------------------------------------------------------------------------------------------------ */
 
 Vector pixel_to_coordinate(Vector& camera, double x, double y, double dist_from_screen, int H, int W){
-    return Vector(camera[0] + x + 0.5 - W/2, camera[1] + y + 0.5 - H/2, camera[2] - dist_from_screen);
+    return Vector(camera.x + x + 0.5 - W/2, camera.y + y + 0.5 - H/2, camera.z - dist_from_screen);
 };
 
 
@@ -687,31 +633,27 @@ Get a random vector shooting off a point on a surface with a normal vector N
 
 Vector random_cos(Vector N){
     // get r1,r2 ~ U([0,1])
-    double r1 = ((double) rand() / (RAND_MAX));
-    double r2 = ((double) rand() / (RAND_MAX));
-
-    double x = cos(2*M_PI*r1)*sqrt(1-r2);
-    double y = sin(2*M_PI*r1)*sqrt(1-r2);
-    double z = sqrt(r2);
+    double r1 = ((double) rand() / (RAND_MAX)), r2 = ((double) rand() / (RAND_MAX));
+    double x = cos(2*M_PI*r1)*sqrt(1-r2), y = sin(2*M_PI*r1)*sqrt(1-r2), z = sqrt(r2);
 
     Vector T1;
-    if ((pow(N[0], 2) < pow(N[1], 2)) && (pow(N[0], 2) < pow(N[2], 2))){
-        T1[0] = 0;
+    if ((pow(N.x, 2) < pow(N.y, 2)) && (pow(N.x, 2) < pow(N.z, 2))){
+        T1.x = 0;
         // swap the values of the other 2 and negate one of them
-        T1[1] = N[2];
-        T1[2] = -1*N[1];  
+        T1.y = N.z;
+        T1.z = -1*N.y;  
     }
-    else if ((pow(N[1], 2) < pow(N[0], 2)) && (pow(N[1], 2) < pow(N[2], 2))){
-        T1[1] = 0;
+    else if ((pow(N.y, 2) < pow(N.x, 2)) && (pow(N.y, 2) < pow(N.z, 2))){
+        T1.y = 0;
         // swap the values of the other 2 and negate one of them
-        T1[0] = N[2];
-        T1[2] = -1*N[0];  
+        T1.x = N.z;
+        T1.z = -1*N.x;  
     }
     else{
-        T1[2] = 0;
+        T1.z = 0;
         // swap the values of the other 2 and negate one of them
-        T1[0] = N[1];
-        T1[1] = -1*N[0];  
+        T1.x = N.y;
+        T1.y = -1*N.x;  
     }
 
     T1.normalize();
@@ -728,9 +670,7 @@ Sample x and y from N(0, standard_deviation) - to avoid only shooting rays throu
 
 void boxMuller(double standard_deviation, double &x, double &y){
     // get r1,r2 ~ U([0,1])
-    double r1 = ((double) rand() / (RAND_MAX));
-    double r2 = ((double) rand() / (RAND_MAX));
-
+    double r1 = ((double) rand() / (RAND_MAX)), r2 = ((double) rand() / (RAND_MAX));
     x = sqrt(-2 * log(r1))*cos(2 * M_PI*r2)*standard_deviation; 
     y = sqrt(-2 * log(r1))*sin(2 * M_PI*r2)*standard_deviation;
 }
@@ -830,7 +770,7 @@ int main(){
     double gamma = 2.2; 
 
     // initialize camera location
-    Vector camera = Vector(0, 0, 70);
+    Vector camera = Vector(7, 0, 50);
 
     // Set the horizontal field of view
     double fov = 60.0;
@@ -844,11 +784,11 @@ int main(){
     Sphere* sphere3 = new Sphere(Vector(-15, -5, 30), 5, Vector(1, 1, 0), "none", 8);
 
     // Load the cat object
-    const char* filename = "Cat/cat.obj";
+    const char* filename = "Knuckles/knuckles.obj";
     TriangleMesh* cat = new TriangleMesh();
     cat -> readOBJ(filename);
     cat -> is_mirror = true; 
-    cat -> resize(0.5, Vector(0, -10, 0));
+    cat -> resize(2, Vector(0, -10, 30));
  
     // Set the scene
     std::vector<Geometry*> objects_in_room; 
@@ -864,8 +804,8 @@ int main(){
     objects_in_room.push_back(cat);
     Scene* scene = new Scene(objects_in_room);
 
-    int max_depth = 2; 
-    int num_paths = 2; 
+    int max_depth = 5; 
+    int num_paths = 5; 
     double stdv = 0.5; 
 
     #pragma omp parallel for schedule(dynamic, 1)
@@ -886,19 +826,19 @@ int main(){
 
             pixel_color = pow(num_paths,-1) * pixel_color; 
             // gamma correction
-            pixel_color[0] = pow(pixel_color[0], 1/gamma);
-            pixel_color[1] = pow(pixel_color[1], 1/gamma);
-            pixel_color[2] = pow(pixel_color[2], 1/gamma);
+            pixel_color.x = pow(pixel_color.x, 1/gamma);
+            pixel_color.y = pow(pixel_color.y, 1/gamma);
+            pixel_color.z = pow(pixel_color.z, 1/gamma);
             // scaling
             pixel_color = 255 * pixel_color; 
             // update the pixel on the image
-            image[y*W*3 + x*3 + 0] = std::min(255., pixel_color[0]);
-            image[y*W*3 + x*3 + 1] = std::min(255., pixel_color[1]);
-            image[y*W*3 + x*3 + 2] = std::min(255., pixel_color[2]);
+            image[y*W*3 + x*3 + 0] = std::min(255., pixel_color.x);
+            image[y*W*3 + x*3 + 1] = std::min(255., pixel_color.y);
+            image[y*W*3 + x*3 + 2] = std::min(255., pixel_color.z);
         }
     }
     // save the image
-    stbi_write_jpg("Images/cat.jpg", W, H, 3, image, W * sizeof(int));
+    stbi_write_jpg("Images/knuckles.jpg", W, H, 3, image, W * sizeof(int));
 
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start); 
